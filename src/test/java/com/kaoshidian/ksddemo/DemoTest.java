f613b10083df1df44a27d4da6ec7a1400d75463a
shurrik <darkforest123@gmail.com>
2014-01-14 16:48:44 +0800

    增加测试用例

---
+++ src/test/java/com/kaoshidian/ksddemo/DemoTest.java
@@ -0,0 +1,13 @@
+package com.kaoshidian.ksddemo;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class DemoTest extends JUnitBase{
+	@Test
+	public void testFindById(){
+		
+		Assert.assertTrue(1==1);
+	}
+}
---
+++ src/test/java/com/kaoshidian/ksddemo/JUnitBase.java
@@ -0,0 +1,5 @@
+package com.kaoshidian.ksddemo;
+
+public class JUnitBase {
+
+}
